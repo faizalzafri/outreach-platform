b577fcf1c3cd06003fd71b38bcd60864f0df0a91
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-03-25 16:46:04 +0530

    Update feedback.component.ts

--- angular/outreach-fms-dashboard/src/app/components/feedback/feedback.component.ts
+++ angular/outreach-fms-dashboard/src/app/components/feedback/feedback.component.ts
@@ -11,13 +11,104 @@ export class FeedbackComponent implements OnInit {
   faAngry = faAngry;
   faFrown = faFrown;
   faMeh = faMeh;
-  faSmile =faSmile;
+  faSmile = faSmile;
   faGrinAlt = faGrinAlt;
 
+  isangActive: boolean = false;
+  isfroActive: boolean = false;
+  ismehActive: boolean = false;
+  issmiActive: boolean = false;
+  isgriActive: boolean = false;
+
+  score: number = 0;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  iconAClicked(event: Event) {
+
+    //reset color and color
+    this.isfroActive = false;
+    this.ismehActive = false;
+    this.issmiActive = false;
+    this.isgriActive = false;
+    this.score = 0;
+    
+    //change color
+    this.isangActive = !this.isangActive;
+
+    //set score
+    this.score = this.isangActive ? 1 : 0;
+
+    console.log(this.score)
+
+  }
+  iconFClicked(event: Event) {
+
+    //reset color and score
+    this.ismehActive = false;
+    this.issmiActive = false;
+    this.isgriActive = false;
+    this.isangActive = false;
+    this.score = 0;
+
+    //change color
+    this.isfroActive = !this.isfroActive;
+
+    //set score
+    this.score = this.isfroActive ? 2 : 0;
+    console.log(this.score)
+  }
+  iconMClicked(event: Event) {
+
+    //reset color and score
+    this.isfroActive = false;
+    this.issmiActive = false;
+    this.isgriActive = false;
+    this.isangActive = false;
+    this.score = 0;
+
+    //change color
+    this.ismehActive = !this.ismehActive;
+
+    //set score
+    this.score = this.ismehActive ? 3 : 0;
+    console.log(this.score)
+  }
+  iconSClicked(event: Event) {
+
+    //reset color and score
+    this.isfroActive = false;
+    this.ismehActive = false;
+    this.isgriActive = false;
+    this.isangActive = false;
+    this.score = 0;
+
+    //change color
+    this.issmiActive = !this.issmiActive;
+
+    //set score
+    this.score = this.issmiActive ? 4 : 0;
+    console.log(this.score)
+  }
+  iconGClicked(event: Event) {
+
+    //reset color and score
+    this.isangActive = false;
+    this.isfroActive = false;
+    this.ismehActive = false;
+    this.issmiActive = false;
+    this.score = 0;
+
+    //change color
+    this.isgriActive = !this.isgriActive;
+
+    //set score
+    this.score = this.isgriActive ? 5 : 0;
+    console.log(this.score)
+  }
+
 
 }
