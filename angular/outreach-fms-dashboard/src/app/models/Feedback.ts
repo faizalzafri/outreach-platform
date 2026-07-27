b7bb9179c418aa5b4f2f6703c1b506e070a2f356
Faizal Zafri <34762675+faizalzafri@users.noreply.github.com>
2026-07-27 13:12:27 +0530

    remove legacy UI

--- angular/outreach-fms-dashboard/src/app/models/Feedback.ts
+++
@@ -1,8 +0,0 @@
-export interface Feedback {
-    eventId: string;
-    employeeId: string;
-    score: number;
-    answer1: string;
-    answer2: string;
-    status: string;
-}
--- angular/outreach-fms-dashboard/src/app/models/FeedbackScore.ts
+++
@@ -1,8 +0,0 @@
-export class FeedbackScore {
-    eventId: string;
-    employeeId: string;
-    score:number;
-    answer1: string;
-    answer2: string;
-    status: string;
-}
